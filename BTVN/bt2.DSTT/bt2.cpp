77c7fc151dfd18e2acf3e69c43d2d62fce97d7c6
Khoa Tran <44254391+ankhoa18599@users.noreply.github.com>
2019-03-25 19:25:03 +0700

    .

--- BTVN/bt2.DSTT/bt2.cpp
+++ BTVN/bt2.DSTT/bt2.cpp
@@ -1,26 +1,30 @@
-#include<iostream>
+#include <iostream>
 #include <conio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <iomanip>
 //#define MAX 100
 using namespace std;
 
 typedef struct SinhVien
 {
-	char maso[];
-	char ho[];
-	char ten[];
+	char maso[15];
+	char ho[10];
+	char ten[10];
 };
+int n;
+SinhVien *a=new SinhVien[n];
 void nhap(SinhVien &a)
 {
 	cout<<"Nhap ma so cua sinh vien:";
 	fflush(stdin);
-	gets(a.maso);
+	cin.getline(a.maso,15);
 	cout<<"Nhap ho cua sinh vien:";
 	fflush(stdin);
-	gets(a.ho);
+	cin.getline(a.ho,10);
 	cout<<"Nhap ten cua sinh vien:";
 	fflush(stdin);
-	gets(a.ten);
+	cin.getline(a.ten,10);
 	
 }
 void nhapN(SinhVien a[], int n)
@@ -52,12 +56,41 @@ void xemN(SinhVien a[], int n )
 		xem(a[i]);
 	}
 }
+
+void Swap(SinhVien &c, SinhVien &d)
+{
+	SinhVien temp;
+	temp=c;
+	c=d;
+	d=temp;
+	
+}
+
+void Sort_name(SinhVien a[], int n)
+{
+	int t,h;
+	for(int i=0; i<n-1;i++)
+		for(int j=i+1; j<n;j++)
+			{
+				t=strcmp(a[i].ten,a[j].ten);
+					if(t>0)
+						Swap(a[i],a[j]);
+					else if(t==0)
+					{
+						h=strcmp(a[i].ho,a[j].ho);
+						if(h>0)
+							Swap(a[i],a[j]);		
+					}
+			}
+}
+
 int main()
 {
-	int n;
 	cout<<"Nhap so luong sinh vien: "; cin>>n;
-	SinhVien *a=new SinhVien[n];
 	nhapN(a,n);
+	xemN(a,n);
+	Sort_name(a,n);
+	cout<<"\nSau khi sap xep:";
 	xemN(a,n);
 	return 0;
 }
